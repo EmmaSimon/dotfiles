2c8f61e1708ad056ff266c6a18a7f356d457cb97
Emma Simon <esimon@kmbs.konicaminolta.us>
2016-11-15 16:34:53 -0500

    Update fish settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,6 @@
-function hopup
-	hopi c554.sep.net web upload 1234567812345678 0A0243A9 app app/$argv (for i in (git ls-files -m); basename $i; end)
-end
-
-function hopall
-    hopi c554.sep.net web upload 1234567812345678 0A0243A9 app app/$argv
-end
-
 function fish_greeting
-	fortune
+    fortune-goat
+    echo
 end
 
 set TF_ALIAS fuck
@@ -23,8 +16,84 @@ function fuck -d 'Correct your previous console command'
     end
 end
 
-alias pybugs='python2 /home/emma/pybugs/pybugs'
+function bug
+    eval python2 /home/emma/src/pybugs/pybugs $argv
+end
+
+function buglist
+    set split 0
+    set path (realpath ~/tmp/buglist)
+    set args (getopt -a -s sh -o sp: -l split,path: -- $argv); or return
+    set args (fish -c "for el in $args; printf \"%s\n\" \"\$el\"; end")
+
+    set i 1
+    while true
+        switch $args[$i]
+            case "-s" "--split"
+                set split 1
+            case "-p" "--path"
+                set i (math "$i + 1")
+                if ls $args[$i] > /dev/null ^ /dev/null
+                    set path $args[$i]
+                end
+            case "--"
+                set i (math "$i + 1")
+                if math "$i" \> (count $args) > /dev/null ^ /dev/null;
+                    break
+                end
+                if ls $args[$i] > /dev/null ^ /dev/null
+                    set path $args[$i]
+                else
+                    set_color red
+                    echo "Could not read file" $args[$i]
+                    set_color yellow
+                    echo "Falling back to $path"
+                    set_color normal
+                end
+                break
+        end
+        set i (math "$i + 1")
+    end
+    set bugs (cat $path)
+
+    if math $split > /dev/null ^ /dev/null
+        for b in $bugs
+            if echo $b | grep -v -e '^[[:space:]]*$' > /dev/null ^ /dev/null
+                echo $b | cut -d '-' -f2
+            end
+        end
+        return
+    end
+    for b in $bugs
+        set line $line (echo $b | cut -d '-' -f2)
+    end
+    echo $line
+end
+
+function notebugs
+    if test (count $argv) = 0
+        set bugs (buglist -s)
+    else if ls $argv[1] > /dev/null ^ /dev/null
+        set bugs (buglist $argv[1] -s)
+    else if math (count $argv) \> 0 > /dev/null ^ /dev/null
+        set bugs $argv
+    end
+
+    for b in $bugs
+        bug note $b
+    end
+end
+
+alias pybugs=bug
 alias python=python3
-export EDITOR='subl -n -w'
-export mfp_ip='c554.sep.net'
-set mfp_ip 'c554.sep.net'
+alias appbuild='python /home/emma/src/bmp/pack/app.py bundle'
+alias manifest='python /home/emma/src/bmp/pack/manifest_generator.py'
+alias bmppack='python2 /home/emma/src/bmp/tada/app.py'
+
+set -x PAGER 'most'
+set -x VISUAL 'subl -nw'
+set -x EDITOR $VISUAL
+set -x PATH $PATH /usr/local/go/bin $HOME/src/go/bin $HOME/bin $HOME/src/npm-global/bin
+set -x GOPATH $HOME/src/go
+set -x GOBIN $HOME/src/go/bin
+set -x mfp_ip 'c554.sep.net'
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,25 +1,7 @@
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_show_informative_status 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
-
-function fish_prompt --description 'Write out the prompt'
-	if test -z $WINDOW
-   printf '%s%s %s%s%s%s > ' (set_color CC66FF) (echo \U2643) (set_color 6699FF) (prompt_pwd) (set_color normal) (__fish_git_prompt)
- else
-   printf '%s%s %s(%s)%s%s%s%s > ' (set_color CC66FF) (echo \U2643) (set_color white) (echo $WINDOW) (set_color 6699FF) (prompt_pwd) (set_color normal) (__fish_git_prompt)
- end
+function fish_prompt
+ printf '%s\U2643 %s%s%s > '\
+   (set_color blue)\
+   (set_color cyan)\
+   (prompt_pwd)\
+   (set_color normal)
 end
