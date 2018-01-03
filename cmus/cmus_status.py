2066684fbf8dd2744ae5a4219134565c37e49564
Emma Simon <esimon@kmbs.konicaminolta.us>
2018-01-03 16:59:40 -0500

    Update my cmus gnome notification script

--- cmus/cmus_status.py
+++ cmus/cmus_status.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+# Based on https://github.com/cmus/cmus/wiki/cmus_status.py
+# Switches to python3, and adds back, forward, and pause buttons
+# Requirements:
+#  * PyGObject - https://pygobject.readthedocs.io/en/latest/getting_started.html
+# Optional (for image display):
+#  * Pillow - pip install Pillow
+#  * mutagen - pip install mutagen
 
 import gi
 import os.path
@@ -24,11 +31,26 @@ if d.get('album'):
     display = '{} ({})'.format(display, d.get('album'))
 
 try:
-    image = GdkPixbuf.Pixbuf.new_from_file(
-        '/home/emma/.config/cmus/notification_icon.png'
-    )
+    from io import BytesIO
+    from mutagen import File
+    from PIL import Image
+
+    temp_image = '/tmp/cmus_album_art.png'
+    file = File(d.get('file'))
+    try:
+        art = BytesIO(file.tags.getall('APIC')[0].data);
+    except:
+        art = BytesIO(file.pictures[0].data);
+    pic = Image.open(art)
+    pic.save(temp_image)
+    image = GdkPixbuf.Pixbuf.new_from_file(temp_image)
 except:
-    image = None
+    try:
+        # Set a default icon here
+        fallback_image_path = '/path/to/notification_icon.png'
+        image = GdkPixbuf.Pixbuf.new_from_file(fallback_image_path)
+    except:
+        image = None
 
 
 def cmus_callback(notification, action, *args, **kwargs):
@@ -38,20 +60,24 @@ def cmus_callback(notification, action, *args, **kwargs):
         call(['cmus-remote', '-r'])
     elif action == 'pause':
         call(['cmus-remote', '-u'])
-    GLib.MainLoop().quit()
 
+def close_callback(*args, **kwargs):
+    main.quit()
 
+notification_timeout = 3000;
 Notify.init("cmus-status")
+main = GLib.MainLoop()
 n = Notify.Notification.new(title, display)
-n.set_timeout(3000)
+n.set_timeout(notification_timeout)
 n.set_urgency(0)
 if image:
-    n.set_icon_from_pixbuf(image)
     n.set_image_from_pixbuf(image)
 n.set_hint('transient', GLib.Variant.new_boolean(True))
 n.set_hint('suppress-sound', GLib.Variant.new_boolean(True))
 n.add_action('prev', 'Previous', cmus_callback, None)
 n.add_action('pause', 'Pause', cmus_callback, None)
 n.add_action('next', 'Next', cmus_callback, None)
+n.connect('closed', close_callback)
 n.show()
-GLib.MainLoop().run()
+
+main.run()
